ec500be65721ae6103cbd8922688c002c2c3312b
MAHAMAT BARKAI <mahamatbarkai769@gmail.com>
2025-12-18 00:40:22 +0100

    Add Etudiant class with registration and save methods

---
+++ LSP/AvantRefactoring.java
@@ -0,0 +1,9 @@
+public class Etudiant {
+    public void inscrire() {
+        System.out.println("Inscription de l'étudiant");
+    }
+
+    public void sauvegarderBD() {
+        System.out.println("Sauvegarde dans la base de données");
+    }
+}
