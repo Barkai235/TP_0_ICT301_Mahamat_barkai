4d9366023465eac958a170e2d050d31c7b13832b
MAHAMAT BARKAI <mahamatbarkai769@gmail.com>
2025-12-18 00:42:22 +0100

    Add Etudiant and EtudiantRepository classes

---
+++ LSP/ApresRefactoring.java
@@ -0,0 +1,11 @@
+class Etudiant {
+    public void inscrire() {
+        System.out.println("Inscription de l'étudiant");
+    }
+}
+
+class EtudiantRepository {
+    public void sauvegarder() {
+        System.out.println("Sauvegarde dans la base de données");
+    }
+}
